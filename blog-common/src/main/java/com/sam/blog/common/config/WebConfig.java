7a51d491aa51425ec7bddf8fe669a1187900b761
samliw <1452308447@qq.com>
2022-04-10 23:17:58 +0800

    add swagger 2 访问swagger 后台不会有异常

--- blog-common/src/main/java/com/sam/blog/common/config/WebConfig.java
+++ blog-common/src/main/java/com/sam/blog/common/config/WebConfig.java
@@ -1,11 +1,14 @@
 package com.sam.blog.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 /**
- *  配置web config
+ *  配置WebMvcConfigurer
+ *   运行访问swagger 不需要登录验证
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -15,5 +18,9 @@ public class WebConfig implements WebMvcConfigurer {
         // 解决 SWAGGER 404报错
         registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LocaleChangeInterceptor());
     }
 }
